418cdb9d6e1d84d095d4c5df74f0744ff19c7411
Travis Collins <travisfcollins@gmail.com>
2012-10-04 14:26:52 -0400

    add ld library path

--- GNURadio/OFDM/GNU_Mat_BLISS.m
+++ GNURadio/OFDM/GNU_Mat_BLISS.m
@@ -1,13 +1,13 @@
 %% OFDM BLISS
 
 % First Blind Look
-!LD_
+!LD_LIBRARY_PATH="" &&
 interfere_org=read_complex_binary('BLIND.txt');
 
 % Run receiver
-!LD_
+!LD_LIBRARY_PATH="" &&
 mixed_input=read_complex_binary('mixed.txt');
 bliss_SS( mixed_input,interfere_org, fft_size );
 
 % Run Demod and Eval
-!LD_
+!LD_LIBRARY_PATH="" &&
